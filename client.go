d4ab5876ef192978aea4dc303b6b6ad2d475f558
Ram <ram@Apekshas-MacBook-Air.local>
2019-10-17 09:10:09 -0400

    commit torrent

--- client.go
+++ client.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+type pipeMetadata struct {
+	path string
+}
+
+func fileInfo(path string) (err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+	statData, err := file.Stat()
+	log.Printf("%#v", statData)
+
+	return
+}
+
+func main() {
+	fileInfo("./client.go")
 }
